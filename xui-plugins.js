c87c72473b8cb4c8d933a1d650e22090429e0453
remy <remy@remysharp.com>
2010-06-18 17:17:28 +0200

    Updated ui a little - still need to do the green racing post highscores

---
+++ xui-plugins.js
@@ -0,0 +1,49 @@
+(function ($) {
+var down,
+    touchSupport = ('createTouch' in document);
+    
+document.body.ontouchmove = function (event) {
+  var over;
+
+  if (down !== false) {
+    over = document.elementFromPoint(event.targetTouches[0].pageX, event.targetTouches[0].pageY);
+    
+    if (!elementIn(over, down[0]) && over !== down[0]) {
+      down.removeClass('active');
+    } else {
+      down.addClass('active');
+    }
+  }
+
+  // just to be sure
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+};
+
+function elementIn(element, context) {
+  do {
+    if (element === context) return true;
+  } while (element = element.parentNode);
+  return false;
+}
+
+$.fn.touch = function (fn) {
+  return touchSupport ? this.on('touchstart', function () {
+    down = $(this).addClass('active');
+  }).on('touchend', function (event) {
+    down.removeClass('active');
+    down = false;
+
+    var over = document.elementFromPoint(event.changedTouches[0].pageX, event.changedTouches[0].pageY);
+    if (over === this || elementIn(over, this)) {
+      fn.call(this, event);
+    }
+  }) : this.on('click', fn);
+};
+
+$.fn.touchClick = function (fn) {
+  return this.on('touchstart', fn).on('click', fn);
+};
+  
+})(xui);
