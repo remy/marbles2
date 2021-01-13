e64903fa738a76bf78c2baa23124287cf5856b9b
Remy Sharp <remy@remysharp.com>
2021-01-13 06:53:36 +0000

    Update sw

--- app/retro/sw.js
+++ app/retro/sw.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'vC';
+const cacheName = 'vd';
 
 self.addEventListener('install', e => {
   // once the SW is installed, go ahead and fetch the resources
